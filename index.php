6bb97b5527f746f83245a6563fac958a4e0abcf5
kritika58 <31966913+kritika58@users.noreply.github.com>
2018-06-04 13:03:55 +0530

    tweets.php

--- index.php
+++ index.php
@@ -15,6 +15,11 @@ $result = $conn->query($sql);
   <link rel="icon" type="image/png" href="https://excellence.qa/wp-content/uploads/2016/12/qatar-foundation.png">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <!-- jQuery library -->
+  <script src="libs/js/jquery.js"></script>
+  <!-- bootstrap JavaScript -->
+  <script src="libs/js/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="libs/js/bootstrap/docs-assets/js/holder.js"></script>
 </head>
 <body>
 
@@ -50,76 +55,42 @@ $result = $conn->query($sql);
     }
     ?>
 
-<!--      <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search Blog..">
-        <span class="input-group-btn">
-          <button class="btn btn-default" type="button">
-            <span class="glyphicon glyphicon-search"></span>
-          </button>
-        </span>
-      </div>
+<!--      
 -->      
     </div>
 
     <div class="col-sm-9">
-
-      <h4><small>RECENT POSTS</small></h4>
-
-      <hr>
-      <h2>I Love Food</h2>
-      <h5><span class="glyphicon glyphicon-time"></span> Post by Jane Dane, Sep 27, 2015.</h5>
-      <h5><span class="label label-danger">Food</span> <span class="label label-primary">Ipsum</span></h5><br>
-      <p>Food is my passion. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <br><br>
+	
       
-      <h4><small>RECENT POSTS</small></h4>
-      <hr>
-      <h2>Officially Blogging</h2>
-      <h5><span class="glyphicon glyphicon-time"></span> Post by John Doe, Sep 24, 2015.</h5>
-      <h5><span class="label label-success">Lorem</span></h5><br>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <hr>
+ 
+        <!-- PAGE CONTENT and PHP CODE WILL BE HERE -->  
+        <div class="page-header">
+            <h1> Live Twitter Feed </h1>
+        </div>
+			</div>
+			 
+<!-- call tweets.php -->
+<?php include 'tweets.php'; ?>
+		 
+		</div> <!-- end <div class="container"> -->
+		 
+		
+		
+		 
+		
+		  
+		<!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		 
 
-      <h4>Leave a Comment:</h4>
-      <form role="form">
-        <div class="form-group">
-          <textarea class="form-control" rows="3" required></textarea>
-        </div>
-        <button type="submit" class="btn btn-success">Submit</button>
-      </form>
-      <br><br>
       
-      <p><span class="badge">2</span> Comments:</p><br>
       
-      <div class="row">
-        <div class="col-sm-2 text-center">
-          <img src="bandmember.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-        </div>
-        <div class="col-sm-10">
-          <h4>Anja <small>Sep 29, 2015, 9:12 PM</small></h4>
-          <p>Keep up the GREAT work! I am cheering for you!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <br>
-        </div>
-        <div class="col-sm-2 text-center">
-          <img src="bird.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-        </div>
-        <div class="col-sm-10">
-          <h4>John Row <small>Sep 25, 2015, 8:25 PM</small></h4>
-          <p>I am so happy for you man! Finally. I am looking forward to read about your trendy life. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <br>
-          <p><span class="badge">1</span> Comment:</p><br>
-          <div class="row">
-            <div class="col-sm-2 text-center">
-              <img src="bird.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-            </div>
-            <div class="col-xs-10">
-              <h4>Nested Bro <small>Sep 25, 2015, 8:28 PM</small></h4>
-              <p>Me too! WOW!</p>
-              <br>
-            </div>
-          </div>
-        </div>
-      </div>
+	  
+	  
     </div>
   </div>
 </div>
---
+++ tweets.php
@@ -0,0 +1,155 @@
+
+			<div class="col-lg-4">
+				<!-- TWITTER USER PROFILE INFORMATION WILL BE HERE -->
+				<?php        
+				// keys from your app
+					$oauth_access_token = "996826218476462080-LlaSHEx8sxfmF1F5i7zqw0hBZhoze6b";
+					$oauth_access_token_secret = "sTYW7SLuzzb2gL46a9WVmp7hzPV3GDLuExYkstPjXbPUX";
+					$consumer_key = "SJYzdTnGW3ErTGlkdY7jlzccF";
+					$consumer_secret = "dPBbgksauakXZjZjMbUYyn5Rt7Auo9lI49pHfip9sX5WHz83M3";
+		 
+					// we are going to use "user_timeline"
+					$twitter_timeline = "user_timeline";
+		 
+					// specify number of tweets to be shown and twitter username
+					// for example, we want to show 20 of Taylor Swift's twitter posts
+					$request = array('count' => '100',
+							'screen_name' => 'AJArabic'
+					);
+					
+					// put oauth values in one oauth array variable
+					$oauth = array(
+					'oauth_consumer_key' => $consumer_key,
+					'oauth_nonce' => time(),
+					'oauth_signature_method' => 'HMAC-SHA1',
+					'oauth_token' => $oauth_access_token,
+					'oauth_timestamp' => time(),
+					'oauth_version' => '1.0'
+					);
+					
+					// combine request and oauth in one array
+					$oauth = array_merge($oauth, $request);
+					 
+					// make base string
+					$baseURI="https://api.twitter.com/1.1/statuses/$twitter_timeline.json";
+					$method="GET";
+					$params=$oauth;
+					 
+					$r = array();
+					ksort($params);
+					foreach($params as $key=>$value){
+						$r[] = "$key=" . rawurlencode($value);
+					}
+					$base_info = $method."&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r));
+					$composite_key = rawurlencode($consumer_secret) . '&' . rawurlencode($oauth_access_token_secret);
+					 
+					// get oauth signature
+					$oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
+					$oauth['oauth_signature'] = $oauth_signature;
+					
+					// make request
+					// make auth header
+					$r = 'Authorization: OAuth ';
+					 
+					$values = array();
+					foreach($oauth as $key=>$value){
+						$values[] = "$key=\"" . rawurlencode($value) . "\"";
+					}
+					$r .= implode(', ', $values);
+					 
+					// get auth header
+					$header = array($r, 'Expect:');
+					 
+					// set cURL options
+					$options = array(
+						CURLOPT_HTTPHEADER => $header,
+						CURLOPT_HEADER => false,
+						CURLOPT_URL => "https://api.twitter.com/1.1/statuses/$twitter_timeline.json?". http_build_query($request),
+						CURLOPT_RETURNTRANSFER => true,
+						CURLOPT_SSL_VERIFYPEER => true
+					);
+					// retrieve the twitter feed
+					$feed = curl_init();
+					curl_setopt_array($feed, $options);
+					$json = curl_exec($feed);
+					curl_close($feed);
+					 
+					// decode json format tweets
+					$tweets=json_decode($json, true);
+					
+					// show user information
+					echo "<div class='overflow-hidden'>";
+					 
+						// user data
+						$profile_photo=str_replace("normal", "400x400", $tweets[0]['user']['profile_image_url_https']);
+						$name=$tweets[0]['user']['name'];
+						$screen_name=$tweets[0]['user']['screen_name'];
+						
+						
+						// show other information about the user
+						echo "<div class='text-align-center'>";
+							echo "<div><h2>{$name}</h2></div>";
+							echo "<div><a href='https://twitter.com/{$screen_name}' target='_blank'>@{$screen_name}</a></div>";
+						echo "</div>";
+						 
+						echo "<hr />";
+					echo "</div>";
+				?>
+				
+			</div> <!-- end <div class="col-lg-4"> -->
+		 
+			<div class="col-lg-8">
+				<!-- TWITTER USER FEED WILL BE HERE -->
+				<?php
+					$statuses_count=$tweets[0]['user']['statuses_count'];
+					$followers_count=$tweets[0]['user']['followers_count'];
+					$now = date('Y-m-d H:i');
+					
+					
+					// show tweets
+					foreach($tweets as $tweet)
+					{
+						 
+						// show a tweet
+						echo "<div class='overflow-hidden'>";
+							 
+							 
+							// show tweet content
+							echo "<div class='tweet-text'>";
+								 
+								// show name and screen name
+								echo "<h4 class='margin-top-4px'>";
+									echo "<a href='https://twitter.com/{$screen_name}'>{$name}</a> ";
+									echo "<span class='color-gray'>@{$screen_name}</span>";
+								echo "</h4>";
+								 
+								// show tweet text
+								echo "<div class='margin-zero'>";
+								
+									// get tweet time
+									$tweet_time = $tweet['created_at'];
+									echo $tweet_time;
+									echo nl2br("\n ");
+									
+									// get tweet text
+									$tweet_text=$tweet['text'];
+									 
+									// make links clickable
+									$tweet_text=preg_replace('@(https?://([-\w\.]+)+(/([\w/_\.]*(\?\S+)?(#\S+)?)?)?)@', '<a href="$1" target="_blank">$1</a>', $tweet_text);
+									
+									// filter out the retweets 
+									
+									if(preg_match('/^RT/', $tweet_text) == 0)
+									{
+										// output
+										echo $tweet_text;
+									}
+									
+								echo "</div>";
+							echo "</div>";
+							 
+						echo "</div>";
+						echo "<hr />";
+					}
+				?>
+			</div> <!-- end <div class="col-lg-8"> -->
