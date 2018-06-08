fa6922727dbe003e5651a76b304039f6bc180d3d
kritika58 <31966913+kritika58@users.noreply.github.com>
2018-06-08 17:15:39 +0530

    updated db

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $result = $conn->query($sql);
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <!-- <link rel="stylesheet" type="text/css" href="index_style.css"> -->
+  <link rel="stylesheet" type="text/css" href="scroll.css">
   <link rel="icon" type="image/png" href="https://excellence.qa/wp-content/uploads/2016/12/qatar-foundation.png">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
