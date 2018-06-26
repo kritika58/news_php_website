beebec97c75a6ecd886e768f0405f1f79359f0f6
kritika58 <31966913+kritika58@users.noreply.github.com>
2018-06-27 02:17:30 +0300

    arabic befor status

--- stats_ara.php
+++ stats_ara.php
@@ -1,130 +1,176 @@
+<?php
+   include 'dbconnection.php';
+   ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Site Anatomy</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/png" href="favicon.gif">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <style>
            .heading {
          font-family: "Times New Roman", Times, serif;
          font-weight: bold;
          font-size:30px;
+         color:#428bca;
            }
            .desc {
          font-size: 20px;
          font-family: "Times New Roman", Times, serif;
+         font-weight:bold;
          }
          .t_desc {
          font-size: 22px;
+         font-weight:bold;
+         text-decoration: underline;
          font-family: "Times New Roman", Times, serif;
          }
+         .gsize {
+          font-size: 25px;
+          }
+          thead {
+            color:#428bca;
+            
+          }
   </style>
 </head>
 <center>
 <div class="container">  
-<h2 class="heading">Arabic Site Anatomy</h2>
+<h2 class="heading">Arabic Site Statistics</h2>
+<hr>
 <?php
-   include 'dbconnection.php';
-   ?>
 
-<?php
     $total_count=$conn->query("SELECT * FROM news_arabic");
-    $total_count_row=mysqlI_fetch_array($total_count,MYSQL_NUM);
-    $total_count_val=$total_count_row[0];
+    $total_count_row=mysqli_num_rows($total_count);
 
-    $facebook_count=$conn->query("SELECT COUNT('Facebook Page (https://www.facebook.com/)') FROM news_arabic");
-    $facebook_count_row=mysqli_fetch_assoc($facebook_count);
-    $facebook_count_val=$facebook_count_row["COUNT('Facebook Page (https://www.facebook.com/)')"];
 
-    $twitter_count=$conn->query("SELECT COUNT(user_name) FROM news_arabic");
-    $twitter_count_row=$twitter_count->fetch_array();
-    $twitter_count_val=$twitter_count_row["COUNT(user_name)"];
+    $twitter_count=$conn->query("SELECT distinct user_name FROM news_arabic");
+    $twitter_count_row=mysqli_num_rows($twitter_count);
+    $twitter_count_val=round(($twitter_count_row/$total_count_row)*100,0);
 
-    $rss_count=$conn->query("SELECT COUNT('RSS Feed link') FROM news_arabic");
-    $rss_count_row=$rss_count->fetch_array();
-    $rss_count_val=$rss_count_row["COUNT('RSS Feed link')"];
+    $facebook_count=$conn->query("SELECT distinct `Facebook Page (https://www.facebook.com/)` FROM news_arabic");
+    $facebook_count_row=mysqli_num_rows($facebook_count)-1;
+    $facebook_count_val=round(($facebook_count_row/$total_count_row)*100,0);
+    
 
-    $wiki_count=$conn->query("SELECT COUNT('Wikipedia page (https://ar.wikipedia.org/wiki/)') FROM news_arabic");
-    $wiki_count_row=$wiki_count->fetch_array();
-    $wiki_count_val=$wiki_count_row["COUNT('Wikipedia page (https://ar.wikipedia.org/wiki/)')"];
+    $rss_count=$conn->query("SELECT distinct `RSS Feed link` FROM news_arabic");
+    $rss_count_row=mysqli_num_rows($rss_count)-1;
+    $rss_count_val=round(($rss_count_row/$total_count_row)*100,0);
 
-    $alexa_count=$conn->query("SELECT COUNT('Alexa page (https://www.alexa.com/siteinfo/)') FROM news_arabic");
-    $alexa_count_row=$alexa_count->fetch_array();
-    $alexa_count_val=$alexa_count_row["COUNT('Alexa page (https://www.alexa.com/siteinfo/)')"];
 
-    $yt_count=$conn->query("SELECT COUNT('YouTube (http://www.youtube.com/)') FROM news_arabic");
-    $yt_count_row=$yt_count->fetch_array();
-    $yt_count_val=$yt_count_row["COUNT('YouTube (http://www.youtube.com/)')"];
+    $wiki_count=$conn->query("SELECT distinct `Wikipedia page (https://ar.wikipedia.org/wiki/)` FROM news_arabic");
+    $wiki_count_row=mysqli_num_rows($wiki_count)-1;
+    $wiki_count_val=round(($wiki_count_row/$total_count_row)*100,0);
 
-    $gp_count=$conn->query("SELECT COUNT('GooglePlus (https://plus.google.com/)') FROM news_arabic");
-    $gp_count_row=$gp_count->fetch_array();
-    $gp_count_val=$gp_count_row["COUNT('GooglePlus (https://plus.google.com/)')"];
 
-    $insta_count=$conn->query("SELECT COUNT('Instagram (https://www.instagram.com/)') FROM news_arabic");
-    $insta_count_row=$insta_count->fetch_array();
-    $insta_count_val=$insta_count_row["COUNT('Instagram (https://www.instagram.com/)')"];
+    $alexa_count=$conn->query("SELECT distinct `Alexa page (https://www.alexa.com/siteinfo/)` FROM news_arabic");
+    $alexa_count_row=mysqli_num_rows($alexa_count)-1;
+    $alexa_count_val=round(($alexa_count_row/$total_count_row)*100,0);
+
+
+    $yt_count=$conn->query("SELECT distinct `YouTube (http://www.youtube.com/)` FROM news_arabic");
+    $yt_count_row=mysqli_num_rows($yt_count)-1;
+    $yt_count_val=round(($yt_count_row/$total_count_row)*100,0);
+
+
+    $gp_count=$conn->query("SELECT distinct `GooglePlus (https://plus.google.com/)` FROM news_arabic");
+    $gp_count_row=mysqli_num_rows($gp_count)-1;
+    $gp_count_val=round(($gp_count_row/$total_count_row)*100,0);
+
+
+    $insta_count=$conn->query("SELECT distinct `Instagram (https://www.instagram.com/)` FROM news_arabic");
+    $insta_count_row=mysqli_num_rows($insta_count)-1;
+    $insta_count_val=round(($insta_count_row/$total_count_row)*100,0);
+
                     
 ?>
-
 <h4 class="t_desc">Social Media Profile</h4>        
-  <table style="width:50%" class="desc table table-striped">
-    <thead>
-      <tr>
-        <th>Account</th>
-        <th>News Sources</th>
-      </tr>
-    </thead>
+  <table style="width:50%;border:2px #eee solid;" class="desc table table-striped">
     <tbody>
     <tr>
          <td>Total News Sources</td>
-         <td><?php $total_count_val ?></td>
+         <td><?php echo $total_count_row ?> sources</td>
     </tr>
     <tr>
-         <td>Twitter Accounts</td>
-         <td><?php $twitter_count_val ?></td>
+         <td>&ensp; Twitter Accounts
+         <span style="float:left">
+         <i style="color: #00aced;" class="gsize fa fa-twitter-square"></i>
+         </span>
+         </td>
+         <td><?php echo $twitter_count_row ?>&ensp;(<?php echo $twitter_count_val?>%)</td>
     </tr>
     <tr>
-         <td>Facebook Accounts</td>
-         <td><?php $facebook_count_val ?></td>
+         <td>&ensp; Facebook Accounts
+         <span style="float:left">
+         <i style="color: rgb(59, 89, 152);" class="gsize fa fa-facebook-square"></i>
+         </span>
+         </td>
+         <td><?php echo $facebook_count_row ?>&ensp;(<?php echo $facebook_count_val?>%)</td>
     </tr>
     <tr>
-         <td>Alexa Page Information</td>
-         <td><?php $alexa_count_val ?></td>
+         <td>&ensp; Alexa Page Information
+         <span style="float:left">
+         <i style="color: #000000;" class="gsize fa fa-amazon"></i>
+         </span>
+         </td>
+         <td><?php echo $alexa_count_row ?>&ensp;(<?php echo $alexa_count_val?>%)</td>
     </tr>
     <tr>
-         <td>Instagram Accounts</td>
-         <td><?php $insta_count_val ?></td>
+         <td>&ensp; Instagram Accounts
+         <span style="float:left">
+         <img style="width:25px; height:25px" src="instagram.png">
+         </span>
+         </td>
+         <td><?php echo $insta_count_row ?>&ensp;(<?php echo $insta_count_val?>%)</td>
     </tr>
     <tr>
-         <td>YouTube Accounts</td>
-         <td><?php $yt_count_val ?></td>
+         <td>&ensp; YouTube Accounts
+         <span style="float:left">
+         <i style="color: red" class="gsize fa fa-youtube-play"></i>
+         </span>
+         </td>
+         <td><?php echo $yt_count_row ?>&ensp;(<?php echo $yt_count_val?>%)</td>
     </tr>
     <tr>
-         <td>Google Plus Accounts</td>
-         <td><?php $gp_count_val ?></td>
+         <td>&ensp; Google Plus Accounts
+         <span style="float:left">
+         <img style="width:25px; height:25px" src="googleplus.png">
+         </span>
+         </td>
+         <td><?php echo $gp_count_row ?>&ensp;(<?php echo $gp_count_val?>%)</td>
     </tr>
     <tr>
-         <td>Wikipedia Page</td>
-         <td><?php $wiki_count_val ?></td>
+         <td>&ensp; Wikipedia Pages
+         <span style="float:left;">
+         <img align="left" style="width: 25px; height:25px;" src="wiki.png">
+         </span>
+         </td>
+         <td><?php echo $wiki_count_row ?>&ensp;(<?php echo $wiki_count_val?>%)</td>
     </tr>
     <tr>
-         <td>RSS Feed</td>
-         <td><?php $rss_count_val ?></td>
+         <td>&ensp; RSS Feeds
+         <span style="float:left;">
+        <i style="color: orange;font-size: 25px;" class="fa fa-rss-square"></i></span>
+         </td>
+         <td><?php echo $rss_count_row ?>&ensp;(<?php echo $rss_count_val?>%)</td>
     </tr>
 
     </tbody>
 </table>    
-
 <?php
-$sql_country_count="SELECT COUNT(country), country from news_arabic GROUP BY country ORDER BY COUNT(country) DESC";
+$sql_country_count="SELECT COUNT(country), country,country_code from news_arabic GROUP BY country, country_code ORDER BY `COUNT(country)` DESC";
 $result_country_count = $conn->query($sql_country_count);
 $i=0;
 ?> 
 <h4 class="t_desc">News sources per country</h4>        
-  <table style="width:50%" class="desc table table-striped">
+  <table style="width:50%;border:2px #eee solid;" class="desc table table-striped">
     <thead>
       <tr>
         <th>Country</th>
@@ -137,13 +183,20 @@ $i=0;
         {
             $country_name[]=$row_country_count["country"];
             $country_count[]=$row_country_count["COUNT(country)"];
-            
-            echo "<tr>
-                    <td>$country_name[$i]</td>
-                    <td>$country_count[$i]</td>
+            $flag= strtolower($row_country_count["country_code"]);
+            $flag.=".svg";
+            $f_path="flags\\".$flag;
+            ?>
+            <tr>
+                    <td>&ensp; <?php echo $country_name[$i]?>
+                    <span style="float:left;">
+                    <img style="width:25px;height:25px;" src="<?php echo $f_path ?>">
+                    </span>                    
+                    </td>
+                    <td><?php echo $country_count[$i]?></td>
                     
-                    </tr>";  
-        
+              </tr>
+        <?php
                     $i++;
         }
 
@@ -157,7 +210,7 @@ $i=0;
     $i=0;
     ?>
 <h4 class="t_desc">News sources per category</h4>        
-  <table style="width:50%" class="desc table table-striped">
+  <table style="width:50%;border:2px #eee solid;" class="desc table table-striped">
     <thead>
       <tr>
         <th>Category</th>
@@ -170,13 +223,49 @@ $i=0;
         {
             $category_name[]=$row_category_count["Category"];
             $category_count[]=$row_category_count["COUNT(category)"];
-            
-            echo "<tr>
-                    <td>$category_name[$i]</td>
-                    <td>$category_count[$i]</td>
+
+            $cat= $category_name[$i];
+            if (strcmp($cat,'General')==0)
+            {
+                $fcat='General';
+                $gicon='fa fa-globe';
+            }
+            if (strcmp($cat,'Sports')==0)
+            {
+                $fcat='Sports';
+                $gicon='fa fa-soccer-ball-o';
+            }
+            if (strcmp($cat,'Entertainment')==0)
+            {
+                $fcat='Entertainment';
+                $gicon='fa fa-tv';
+            }
+            if (strcmp($cat,'Science')==0)
+            {
+                $fcat='Science';
+                $gicon='fa fa-cogs';
+            }
+            if (strcmp($cat,'Health')==0)
+            {
+                $fcat='Health';
+                $gicon='fa fa-medkit';
+            }
+            if (strcmp($cat,'Economy')==0)
+            {
+                $fcat='Economy';
+                $gicon='fa fa-money';
+            }
+            ?>
+
+            <tr>
+                    <td>&ensp; <?php echo $category_name[$i]?>
+                    <span style="float:left;">
+                    <i class="gsize <?php echo $gicon?>"></i></span>
+                    </td>
+                    <td><?php echo $category_count[$i]?></td>
                     
-                    </tr>";  
-        
+                    </tr> 
+        <?php 
                     $i++;
         }
 
--- tweets_custom.php
+++ tweets_custom.php
@@ -5,23 +5,25 @@
 	display:inline-block;
 }
 .username {
-	font-size: 25px;
+	font-size: 18px;
 	font-weight:bold;
+	color:gray;
     font-family: "Times New Roman", Times, serif;
 	display: inline-block;
 }
 .tweet {
-	font-size: 20px;
+	font-size: 23px;
     font-family: "Times New Roman", Times, serif;	
 	display: inline-block;
 }
 .tweet_time {
 	font-size: 18px;
+	color:gray;
     font-family: "Times New Roman", Times, serif;			
 	display:inline-block;
 }
 .div_tweet {
-	border: 1px #eee solid;
+	border: 3px #eee solid;
 	min-height: 100px;
 	overflow: auto;
 }
@@ -69,11 +71,10 @@ function time_elapsed_string($datetime,$present, $full = false)
 
    
 <?php
-    include 'dbconnection.php';
+	include 'dbconnection.php';
 	//$sql="SELECT * FROM tweets_ara ORDER BY tweets_ara.date DESC LIMIT 100";
 	$sql="SELECT * FROM tweets_ara, ara_source_name WHERE 
-	ara_source_name.source_user_name = tweets_ara.screen_name";
-	//$sql ="SELECT TOP (30) * FROM Table ORDER BY date DESC";
+	ara_source_name.source_user_name = tweets_ara.screen_name ORDER BY tweets_ara.date DESC LIMIT 100";
 	$result = mysqli_query($conn,$sql);
 	if ($result->num_rows > 0) 
 	{
@@ -96,51 +97,46 @@ function time_elapsed_string($datetime,$present, $full = false)
 					
 			// get tweet text
 			$tweet_text=$tweet['text'];					
-								
+			
+			$org_tweet_text = $tweet_text;
 			// make links clickable
-			$tweet_text=preg_replace('@(https?://([-\w\.]+)+(/([\w/_\.]*(\?\S+)?(#\S+)?)?)?)@', '<a href="$1" target="_blank">Read More &nbsp;</a>', $tweet_text);
+			$tweet_text=preg_replace('@(https?://([-\w\.]+)+(/([\w/_\.]*(\?\S+)?(#\S+)?)?)?)@', '<a href="$1" target="_blank">... &nbsp;</a>', $tweet_text);
+			
+			//$tweet_url = $tweet['entities']['urls'][0]['expanded_url'];
+			
+			
+			
 			//$tweet_text=preg_replace('@(https?://([-\w\.]+)+(/([\w/_\.]*(\?\S+)?(#\S+)?)?)?)@', ' ', $tweet_text );
 													
 			// filter out the retweets 						
 			if(preg_match('/^RT/', $tweet_text) == 0)
-			{							
+			{			
+				echo "<div class=\"div_tweet\">";				
+				
 				// show name and screen name
-				echo "<div class=\"div_tweet\">";
-				echo "<h4 class='margin-top-4px'>";
 				echo "<img class=\"profile_img\" src='{$tweet['user']['profile_image_url_https']}' class='img-thumbnail' />";
 				echo "<html>&ensp;</html>";
-				echo "<a class=\"username\" href='https://twitter.com/{$row['screen_name']}'>{$tweet['user']['name']}</a> ";
+				echo "<p class=\"username\">{$tweet['user']['name']}</p> ";
 				//echo "<span class='color-gray'>@{$screen_name}</span>";
-									
+				
+				// output
+				echo "<h4 class='margin-top-4px'>";
+				echo "<p class=\"tweet\">$tweet_text</p>";
 				echo "</h4>";
 								
 				// get tweet time
-				$tweet_time = $tweet['created_at'];
-				//echo $tweet_time;
-				//echo nl2br("\n ");
-							
+				$tweet_time = $tweet['created_at'];							
 				$t_time= time_elapsed_string($tweet_time,$now);
-				echo "<p class='tweet_time'>$t_time</p>";
+				echo "<p class='tweet_time'>$t_time &nbsp; ($tweet_time)</p>";
 				echo nl2br("\n ");
 								
-				// output
-				echo "<p class=\"tweet\">$tweet_text</p>";
 								
-				$tweet_pic;
 				if (array_key_exists('media', $tweet['entities']))
 				{
-					// get tweet picture
-					//if(array_key_exists('media_url', $tweet['entities']['media']))
-					//{
-						$tweet_pic= $tweet['entities']['media'][0]["media_url"];
-						echo "<img class='tweet_pic' src='{$tweet_pic}' class='img-thumbnail'/>";
-					//}
+					$tweet_pic= $tweet['entities']['media'][0]["media_url"];
+					echo "<img class='tweet_pic' src='{$tweet_pic}' class='img-thumbnail'/>";
 					
 				}
-				/*else 
-				{
-					$tweet_pic = NULL;
-				}*/
 				echo "</div>";
 			}	
 			
@@ -149,8 +145,6 @@ function time_elapsed_string($datetime,$present, $full = false)
 	
 	<?php
 	
-			//echo strlen($row['tweet']).'<br>';
-			//echo $row['tweet'].'<br>';
 			echo '<br>';
 		}
 	}
